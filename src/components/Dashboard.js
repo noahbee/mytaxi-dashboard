c902dd0f217e84b7eba45e54c9fafeb5d7a2ebd2
Noah Banda <35391834+noahbee@users.noreply.github.com>
2022-07-05 12:23:18 +0200

    dashboard styling

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -30,5 +30,26 @@ const Section = styled.section `
 margin-left: 22vw;
 padding: 2rem;
 height: 100%;
+.grid {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  gap: 1rem;
+  margin top: 2rem;
+
+  .row_one {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    height: 50%;
+    gap: 1rem;
+  }
+
+  .row_two {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    height: 50%;
+    gap: 1rem;
+  }
+}
 
 `;
