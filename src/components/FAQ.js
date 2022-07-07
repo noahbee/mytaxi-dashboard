7a079098d23c73860b5495f2d5eaab115bff15f8
Noah Banda <35391834+noahbee@users.noreply.github.com>
2022-07-07 19:29:41 +0200

    faq: cardstyle

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -1,7 +1,64 @@
 import React from 'react'
+import styled from 'styled-components'
+import { IoIosArrowForward } from 'react-icons/io';
+import { AiFillCalendar } from 'react-icons/ai';
+import {MdTimelapse} from 'react-icons/md';
+import { IoMdCash } from 'react-icons/io';
+import { cardStyle } from './ReusableStyles';
 
 export default function FAQ() {
+  const faqs = [
+    {
+      icon: <AiFillCalendar />,
+      text: 'How to manage time',
+    },
+    {
+      icon: <MdTimelapse />,
+      text: 'Regulate transactions over time',
+    },
+    {
+      icon: <IoMdCash />,
+      text: 'Withdraw money',
+    },
+
+  ];
   return (
-    <div>FAQ</div>
+   <Section>
+    <div className='title'>
+      <h2>Infor for drivers</h2>
+    </div>
+    <div className='faqs'>
+      {
+        faqs.map((faq)=> {
+          return (
+            <div className='faq'>
+              <div className='info'>
+              {faq.icon}
+              <h4>{faq.text}</h4>
+            </div>
+            <IoIosArrowForward />
+            </div>
+          )
+        })
+      }
+    </div>
+   </Section>
   )
 }
+
+const Section = styled.section`
+${cardStyle}
+.title {
+  h2{
+    color: #ffc107;
+    font-family: 'Permanent Marker', cursive;
+    letter-spacing: 0.3rem;
+  }
+}
+.faqs {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+}
+
+`;
