db9bb7e104c369092b1253cac486ee9142a3cf00
Noah Banda <35391834+noahbee@users.noreply.github.com>
2022-07-08 15:40:17 +0200

    Faqs section styled

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -59,6 +59,27 @@ ${cardStyle}
   display: flex;
   flex-direction: column;
   gap: 1rem;
+  margin-top: 1rem;
+  .faq {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    cursor: pointer;
+    .info {
+      display: flex;
+      gap: 1rem;
+      align-items: center;
+    }
+    svg {
+      font-size: 1.4rem;
+
+    }
+    &:nth-of-type(2) {
+      border-top: 0.01rem solid #6c6e6e;
+      border-bottom: 0.01rem solid #6c6e6e;
+      padding: 0.8rem 0;
+    }
+  }
 }
 
 `;
